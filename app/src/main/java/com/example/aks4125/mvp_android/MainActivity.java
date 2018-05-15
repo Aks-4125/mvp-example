6a2578d5ecfad4a74582ab5fb7b8073e17954a53
Akash Bhatt <aks4125@gmail.com>
2018-05-15 14:29:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aks4125/mvp_android/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.aks4125.mvp_android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((TextView) findViewById(R.id.tvMessage)).setText("Hello Android");
+
+    }
+}
